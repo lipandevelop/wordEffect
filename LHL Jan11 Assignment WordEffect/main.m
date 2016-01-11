a709bf3d02d496ffd991f21b381d44fc1d41184c
Li Pan <l.pan.toronto@gmail.com>
2016-01-11 17:24:55 -0500

    First Commit

---
+++ LHL Jan11 Assignment WordEffect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LHL Jan11 Assignment WordEffect
+//
+//  Created by Li Pan on 2016-01-11.
+//  Copyright © 2016 Li Pan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf(")
+    
+    return 0;
+}
