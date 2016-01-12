c19f5bab6c4047220e2a4ae2543d7acfef32f214
Li Pan <l.pan.toronto@gmail.com>
2016-01-11 19:55:38 -0500

    Commit Progress 2

--- LHL Jan11 Assignment WordEffect/main.m
+++ LHL Jan11 Assignment WordEffect/main.m
@@ -12,36 +12,50 @@ int main(int argc, const char * argv[]) {
     
     char inputChar[255];
     
+    
     printf("Your Sentence\n>");
-    scanf("%c", inputChar);
+//  sscanf("%s\n", inputChar);
+    fgets(inputChar, 225, stdin);
     
-    printf("%s\n", inputChar);
-    
-    printf("this is my sentence");
+    //printf("%s\n", inputChar);
     
     int selection;
-    printf("1.Make it Louder\n2.Lower Case\n3.Numberize\n4.Canadianize\n5.Respond\n6.De-Space-It\n>");
-    scanf("%d", &selection);
+    printf("How would you like to modify:\n1.Make it Louder\n2.Lower Case\n3.Numberize\n4.Canadianize\n5.Respond\n6.De-Space-It\n%s\n>", inputChar);
     
+    scanf("%d", &selection);
+    printf("%d\n", selection);
     NSString *inputString = [NSString stringWithUTF8String:inputChar];
     
-    if (selection == 1) {
-        inputString = [inputString uppercaseString];
-        NSLog(@"%@", inputString);
-        
-    }
-    else if (selection == 2) {
-        inputString = [inputString lowercaseString];
-        NSLog(@"%@", inputString);
-    }
-    else if (selection == 3) {
-        int value = [inputString intValue];
-        NSLog(@"%d", value);
-    }
-    else if (selection == 4) {
-        inputString = [inputString stringByAppendingString:@", eh?"];
-    }
-
+    printf("%d", selection);
+    NSLog(@"%@", inputString);
     
+    while (selection >6) {
+    
+        if (selection == 1) {
+            inputString = [inputString uppercaseString];
+            NSLog(@"%@", inputString);
+        
+        }
+        else if (selection == 2) {
+            inputString = [inputString lowercaseString];
+            NSLog(@"%@", inputString);
+        }
+        else if (selection == 3) {
+            int value = [inputString intValue];
+            NSLog(@"%d", value);
+        }
+        else if (selection == 4) {
+            inputString = [inputString stringByAppendingString:@", eh?"];
+            NSLog(@"%@", inputString);
+        }
+        else if (selection == 5) {
+            
+        }
+        else if (selection == 6) {
+            inputString = [inputString stringByReplacingOccurrencesOfString:@"1" withString:@"-"];
+            NSLog(@"%@", inputString);
+        }
+
+    }
     return 0;
 }
