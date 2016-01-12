e6bac4f968910b03755a164905d970b5e705c011
Li Pan <l.pan.toronto@gmail.com>
2016-01-11 21:58:37 -0500

    Submission Version

--- LHL Jan11 Assignment WordEffect/main.m
+++ LHL Jan11 Assignment WordEffect/main.m
@@ -8,28 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
-    
+
     char inputChar[255];
     
     
     printf("Your Sentence\n>");
-//  sscanf("%s\n", inputChar);
+    //sscanf("%s\n", inputChar);
     fgets(inputChar, 225, stdin);
     
-    //printf("%s\n", inputChar);
-    
     int selection;
-    printf("How would you like to modify:\n1.Make it Louder\n2.Lower Case\n3.Numberize\n4.Canadianize\n5.Respond\n6.De-Space-It\n%s\n>", inputChar);
+    
+    while (selection <7) {
+    //printf("%s", inputChar);
+    printf("1.Make it Louder\n2.Lower Case\n3.Numberize\n4.Canadianize\n5.Respond\n6.De-Space-It\nHow do you want to manipulate this string?\n>");
     
     scanf("%d", &selection);
-    printf("%d\n", selection);
+        
     NSString *inputString = [NSString stringWithUTF8String:inputChar];
-    
-    printf("%d", selection);
     NSLog(@"%@", inputString);
-    
-    while (selection != 7) {
+
     
         if (selection == 1) {
             inputString = [inputString uppercaseString];
@@ -59,7 +58,6 @@ int main(int argc, const char * argv[]) {
             NSString *newString = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
             NSLog(@"%@", newString);
         }
-
     }
     return 0;
 }
